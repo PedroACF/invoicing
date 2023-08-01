e3c0e7f63038640b25242b0583edacc4e00aa7c4
PedroACF <pedro.acf.3@gmail.com>
2023-08-01 08:37:06 -0400

    s

--- src/Models/SYS/DelegateToken.php
+++ src/Models/SYS/DelegateToken.php
@@ -2,10 +2,22 @@
 
 namespace PedroACF\Invoicing\Models\SYS;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class DelegateToken extends Model
 {
     protected $table = 'sys_delegate_tokens';
+    public function getRemainingDaysAttribute(){
+        $now = new Carbon();
+        $now->startOfDay();
+        $expired = new Carbon($this->fecha_expiracion);
+        $expired->startOfDay();
+        return $now->diffInDays($expired);
+    }
 
+    public static function getActiveToken(){
+        return DelegateToken::where('activo', 1)
+            ->whereDate('fecha_expiracion', '>', date('Y-m-d'))->first();
+    }
 }
